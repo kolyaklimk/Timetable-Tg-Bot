9c9c78e77fd63cc8cea0911358debabf1e069332
Kolya <kolyaklimk@gmail.com>
2023-07-04 23:14:03 +0300

    Use Telegram.Bot

--- Timetable_Tg_Bot/Program.cs
+++ Timetable_Tg_Bot/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Telegram.Bot;
+
+namespace MyApp 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var botClient = new TelegramBotClient("");
+
+            Console.ReadKey();
+        }
+    }
+}
