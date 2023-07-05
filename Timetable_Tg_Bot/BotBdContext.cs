d4bec0189b4e39fb28ea23e8045d15c46448808a
Kolya <kolyaklimk@gmail.com>
2023-07-05 13:42:29 +0300

    Update BotBdContext.cs

--- Timetable_Tg_Bot/BotBdContext.cs
+++ Timetable_Tg_Bot/BotBdContext.cs
@@ -3,8 +3,8 @@ using Timetable_Tg_Bot.Enities;
 
 public class BotBdContext : DbContext
 {
-    public DbSet<User> Users { get; set; }
-    public DbSet<WorkTime> WorkTimes { get; set; }
+    public DbSet<User> Users => Set<User>();
+    public DbSet<WorkTime> WorkTimes => Set<WorkTime>();
     public BotBdContext() => Database.EnsureCreated();
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
