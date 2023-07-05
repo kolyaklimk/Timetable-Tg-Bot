fdab153d1caac1af383036c749b3a3feb06b6b26
Kolya <kolyaklimk@gmail.com>
2023-07-05 12:16:00 +0300

    Add Entites

---
+++ Timetable_Tg_Bot/Enities/User.cs
@@ -0,0 +1,16 @@
+﻿namespace Timetable_Tg_Bot.Enities;
+
+public class User
+{
+    public User(string userId, string name, string password)
+    {
+        UserId = userId;
+        Name = name;
+        Password = password;
+    }
+    public string UserId { get; set; }
+    public string Name { get; set; }
+    public string Password { get; set; }
+    public List<WorkTime> WorkTime { get; set; } = new();
+    public DateTime Subscription { get; set; } = DateTime.Now;
+}
---
+++ Timetable_Tg_Bot/Enities/WorkTime.cs
@@ -0,0 +1,19 @@
+﻿namespace Timetable_Tg_Bot.Enities;
+
+public class WorkTime
+{
+    public WorkTime(User user, TimeOnly start, TimeOnly end, DateOnly date, bool isBusy)
+    {
+        User = user;
+        Start = start;
+        End = end;
+        Date = date;
+        IsBusy = isBusy;
+    }
+
+    public User User { get; set; }
+    public TimeOnly Start { get; set; }
+    public TimeOnly End { get; set; }
+    public DateOnly Date { get; set; }
+    public bool IsBusy { get; set; }
+}
