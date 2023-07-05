b3eb5787b0e0011a75a1fd5ad847c53001ea650b
Kolya <kolyaklimk@gmail.com>
2023-07-05 19:30:30 +0300

    Create GeneralCommands.cs

---
+++ Timetable_Tg_Bot/TgCommands/GeneralCommands.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TimetableTgBot.TgCommands;
+
+public static class GeneralCommands
+{
+    public static async Task DeleteMessage(ITelegramBotClient botClient, Message message)
+    {
+        try
+        {
+            await botClient.DeleteMessageAsync(message.Chat.Id, message.MessageId);
+        }
+        catch
+        {
+            Console.WriteLine("DeleteMessageAsync error in /start");
+        }
+    }
+}
