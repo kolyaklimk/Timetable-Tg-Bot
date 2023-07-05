970b8411a2919d88e7206873a09b65fee633274a
Kolya <kolyaklimk@gmail.com>
2023-07-05 19:30:17 +0300

    Create Constants.cs

---
+++ Timetable_Tg_Bot/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimetableTgBot;
+
+public class Constants
+{
+    public const string Register = "Регистрация";
+    public const string LogIn = "Войти";
+    public const string EnterName = "Введите имя";
+    public const string EnterPassword = "Введите пароль";
+}
