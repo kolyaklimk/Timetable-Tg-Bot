5770c81604dd42d019beecf7c9355c3baebae90d
Kolya <kolyaklimk@gmail.com>
2023-07-05 11:08:58 +0300

    Create ApplicationContext for Db

---
+++ Timetable_Tg_Bot/ApplicationContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Telegram.Bot.Types;
+
+public class ApplicationContext : DbContext
+{
+    public ApplicationContext() => Database.EnsureCreated();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=users.db");
+    }
+}
