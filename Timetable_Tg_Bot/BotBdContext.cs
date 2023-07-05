32b952ba5dc91849626d5dd4896a28fc206efb87
Kolya <kolyaklimk@gmail.com>
2023-07-05 13:10:39 +0300

    Rename Context

---
+++ Timetable_Tg_Bot/BotBdContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Timetable_Tg_Bot.Enities;
+
+public class BotBdContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<WorkTime> WorkTimes { get; set; }
+    public BotBdContext() => Database.EnsureCreated();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=bot.db");
+    }
+}
