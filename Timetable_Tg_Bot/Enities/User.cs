b419c31f55c5bcb39965033b2ac48527390a6ec6
Kolya <kolyaklimk@gmail.com>
2023-07-05 14:25:20 +0300

    Update Db for correct work

--- Timetable_Tg_Bot/Enities/User.cs
+++ Timetable_Tg_Bot/Enities/User.cs
@@ -1,10 +1,13 @@
-﻿namespace Timetable_Tg_Bot.Enities;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Timetable_Tg_Bot.Enities;
 
 public class User
 {
-    public string UserId { get; set; }
+    public int Id { get; set; }
     public string Name { get; set; }
     public string Password { get; set; }
-    public List<WorkTime> WorkTimes { get; set; } = new();
+    public ICollection<WorkTime> WorkTimes { get; } = new List<WorkTime>();
     public DateTime Subscription { get; set; }
 }
--- Timetable_Tg_Bot/Enities/WorkTime.cs
+++ Timetable_Tg_Bot/Enities/WorkTime.cs
@@ -1,10 +1,14 @@
-﻿namespace Timetable_Tg_Bot.Enities;
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Timetable_Tg_Bot.Enities;
 
 public class WorkTime
 {
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public User User { get; set; } = null!;
     public TimeOnly Start { get; set; }
     public TimeOnly End { get; set; }
     public DateOnly Date { get; set; }
     public bool IsBusy { get; set; }
-    public string UserId { get; set; }
 }
