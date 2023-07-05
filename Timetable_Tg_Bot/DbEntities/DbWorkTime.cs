ce5e8185bc56d80074eb2033c975e99b3d85720c
Kolya <kolyaklimk@gmail.com>
2023-07-05 19:30:24 +0300

    Create DbWorkTime.cs

---
+++ Timetable_Tg_Bot/DbEntities/DbWorkTime.cs
@@ -0,0 +1,11 @@
+﻿using TimetableTgBot.Entities;
+
+namespace TimetableTgBot.DbEntities;
+
+public class DbWorkTime : BotDbContext
+{
+    public void Delete(WorkTime workTime)
+    {
+        WorkTimes.Remove(workTime);
+    }
+}
