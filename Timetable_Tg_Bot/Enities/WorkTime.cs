0ece0e07b3fcceb10366a0cf799e24e8a82cefd8
Kolya <kolyaklimk@gmail.com>
2023-07-05 12:58:45 +0300

    Update enities

--- Timetable_Tg_Bot/Enities/WorkTime.cs
+++ Timetable_Tg_Bot/Enities/WorkTime.cs
@@ -2,18 +2,9 @@
 
 public class WorkTime
 {
-    public WorkTime(User user, TimeOnly start, TimeOnly end, DateOnly date, bool isBusy)
-    {
-        User = user;
-        Start = start;
-        End = end;
-        Date = date;
-        IsBusy = isBusy;
-    }
-
-    public User User { get; set; }
     public TimeOnly Start { get; set; }
     public TimeOnly End { get; set; }
     public DateOnly Date { get; set; }
     public bool IsBusy { get; set; }
+    public string UserId { get; set; }
 }
