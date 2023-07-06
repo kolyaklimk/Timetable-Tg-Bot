7495a969401bd92e4c781a79479a148771be9ad2
Kolya <kolyaklimk@gmail.com>
2023-07-06 20:58:02 +0300

    Update GeneralCommands.cs

--- Timetable_Tg_Bot/TgCommands/GeneralCommands.cs
+++ Timetable_Tg_Bot/TgCommands/GeneralCommands.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.InteropServices;
-using Telegram.Bot;
+﻿using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.ReplyMarkups;
 
@@ -7,30 +6,24 @@ namespace TimetableTgBot.TgCommands;
 
 public static class GeneralCommands
 {
-    public static async Task DeleteMessageUser(ITelegramBotClient botClient, Message message, CancellationToken cancellationToken)
+    public static async Task DeleteMessage(ITelegramBotClient botClient, Message message, CancellationToken cancellationToken)
     {
-        try
+        if (message != null)
         {
-            await botClient.DeleteMessageAsync(message.Chat.Id, message.MessageId, cancellationToken);
-        }
-        catch
-        {
-            Console.WriteLine("DeleteMessageAsync error in DeleteMessageUser");
+            try
+            {
+                await botClient.DeleteMessageAsync(message.Chat.Id, message.MessageId, cancellationToken);
+            }
+            catch { }
         }
     }
 
     public static async Task CreateMenu(ITelegramBotClient botClient, Message message, CancellationToken cancellationToken)
     {
-        var replyKeyboardMarkup = new  InlineKeyboardMarkup(new[]
-        {
-            new InlineKeyboardButton[] { Constants.ImageMenu, Constants.TimeTableMenu },
-            new InlineKeyboardButton[] { Constants.SupportMenu, Constants.SubscribeMenu }
-        }); 
-        
         await botClient.SendTextMessageAsync(
             message.Chat.Id,
             "Меню:",
-            replyMarkup: replyKeyboardMarkup,
+            replyMarkup: Constants.MenuMarkup,
             cancellationToken: cancellationToken);
     }
 }
