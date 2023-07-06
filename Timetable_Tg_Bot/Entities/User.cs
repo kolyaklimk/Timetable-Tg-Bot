1300fd61cf3b2d43e5f6f30fda91085931b91ac3
Kolya <kolyaklimk@gmail.com>
2023-07-06 11:46:15 +0300

    Update User.cs

--- Timetable_Tg_Bot/Entities/User.cs
+++ Timetable_Tg_Bot/Entities/User.cs
@@ -3,7 +3,9 @@
 public class User
 {
     public long Id { get; set; }
-    public string Name { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? UserName { get; set; }
     public ICollection<WorkTime> WorkTimes { get; } = new List<WorkTime>();
     public DateTime Subscription { get; set; }
 }
