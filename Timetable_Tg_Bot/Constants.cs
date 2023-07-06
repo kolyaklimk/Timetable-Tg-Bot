56329f91f517be9f1dc7adeb717e60c4a57a4ef9
Kolya <kolyaklimk@gmail.com>
2023-07-06 20:56:44 +0300

    Update Constants.cs

--- Timetable_Tg_Bot/Constants.cs
+++ Timetable_Tg_Bot/Constants.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Telegram.Bot.Types.ReplyMarkups;
 
 namespace TimetableTgBot;
 
@@ -19,5 +15,27 @@ public class Constants
     #region Dates
     public const string Month = "Месяц";
     public const string Week = "Неделя";
+    #endregion
+
+    #region Other
+    public static readonly InlineKeyboardMarkup MenuMarkup = new InlineKeyboardMarkup(new[]
+    {
+        new InlineKeyboardButton[] {
+            ImageMenu,
+            InlineKeyboardButton.WithCallbackData("Расписание", $"ChooseMonthTimeTable_{DateTime.Now.Month}_{DateTime.Now.Year}") },
+
+            new InlineKeyboardButton[] {
+                SupportMenu,
+                SubscribeMenu }
+    });
+
+    public static readonly InlineKeyboardButton[] WeekButtons = new InlineKeyboardButton[] {
+        InlineKeyboardButton.WithCallbackData("Пн", "\0"),
+        InlineKeyboardButton.WithCallbackData("Вт", "\0"),
+        InlineKeyboardButton.WithCallbackData("Ср", "\0"),
+        InlineKeyboardButton.WithCallbackData("Чт", "\0"),
+        InlineKeyboardButton.WithCallbackData("Пт", "\0"),
+        InlineKeyboardButton.WithCallbackData("Сб", "\0"),
+        InlineKeyboardButton.WithCallbackData("Вс", "\0") };
     #endregion
 }
