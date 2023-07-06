6aa41060450291c6c46bc051e16e49c790440725
Kolya <kolyaklimk@gmail.com>
2023-07-06 20:57:56 +0300

    Add ChooseDateTimeTable

--- Timetable_Tg_Bot/TgBot.cs
+++ Timetable_Tg_Bot/TgBot.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
 using Telegram.Bot;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
@@ -34,14 +35,11 @@ public class TgBot
     async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
     {
         var message = update.Message;
-        var callbackQuery = update.CallbackQuery;
-
-        //var callbackQuery = update.CallbackQuery;
 
         if (update.Type == UpdateType.Message)
         {
             #region /start
-            if (message?.Text == "/start")
+            if (message.Text == "/start")
             {
                 var user = await DbContext.Users.FirstOrDefaultAsync(arg => arg.Id == message.From.Id, cancellationToken);
 
@@ -59,18 +57,40 @@ public class TgBot
                     await DbContext.SaveChangesAsync(cancellationToken);
                 }
 
+                await GeneralCommands.DeleteMessage(botClient, message, cancellationToken);
                 await GeneralCommands.CreateMenu(botClient, message, cancellationToken);
-                await GeneralCommands.DeleteMessageUser(botClient, message, cancellationToken);
                 return;
             }
             #endregion
         }
+
         if (update.Type == UpdateType.CallbackQuery)
         {
-            #region TimeTableMenu
-            if (callbackQuery?.Data == Constants.TimeTableMenu)
-            {
+            var callbackQuery = update.CallbackQuery;
 
+            if (callbackQuery.Data == "\0")
+            {
+                await botClient.AnswerCallbackQueryAsync(callbackQuery.Id);
+                return;
+            }
+
+            #region GoMenu
+            if (callbackQuery?.Data == Constants.GoMenu)
+            {
+                await GeneralCommands.DeleteMessage(botClient, callbackQuery.Message, cancellationToken);
+                await GeneralCommands.CreateMenu(botClient, callbackQuery.Message, cancellationToken);
+                return;
+            }
+            #endregion
+
+            #region ChooseDate
+            if (Regex.IsMatch(callbackQuery?.Data, Constants.ChooseMonthTimeTable))
+            {
+                Match match = Regex.Match(callbackQuery?.Data, Constants.ChooseMonthTimeTable);
+
+                await GeneralCommands.DeleteMessage(botClient, callbackQuery.Message, cancellationToken);
+
+                await TimeTableCommands.ChooseDateTimeTable(match, botClient, callbackQuery.Message, cancellationToken);
                 return;
             }
             #endregion
@@ -102,9 +122,10 @@ public class TgBot
             }
             #endregion
         }
+
         else
         {
-            await GeneralCommands.DeleteMessageUser(botClient, message, cancellationToken);
+            await GeneralCommands.DeleteMessage(botClient, message, cancellationToken);
         }
     }
 
---
+++ Timetable_Tg_Bot/TgCommands/TimeTableCommands.cs
@@ -0,0 +1,81 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TimetableTgBot.TgCommands;
+
+public static class TimeTableCommands
+{
+    private static Dictionary<string, InlineKeyboardMarkup> SavedCalendars = new();
+
+    public static async Task ChooseDateTimeTable(Match match, ITelegramBotClient botClient, Message message, CancellationToken cancellationToken)
+    {
+        InlineKeyboardMarkup markup;
+
+        if (SavedCalendars.ContainsKey(match.Groups[1].Value + "_" + match.Groups[2].Value))
+        {
+            markup = SavedCalendars[match.Groups[1].Value + "_" + match.Groups[2].Value];
+        }
+        else
+        {
+            #region Create calendar
+            DateTime currentDate = DateTime.Parse("1/" + match.Groups[1].Value + "/" + match.Groups[2].Value);
+            int daysInMonth = DateTime.DaysInMonth(currentDate.Year, currentDate.Month);
+            int firstDayOfMonth = ((int)currentDate.DayOfWeek + 6) % 7;
+            var monthName = currentDate.ToString("MMMM", new CultureInfo("ru-RU"));
+
+            // Month and Name day of week
+            List<InlineKeyboardButton[]> rows = new List<InlineKeyboardButton[]>
+            {
+                new InlineKeyboardButton[] {
+                    InlineKeyboardButton.WithCallbackData(char.ToUpper(monthName[0]) + monthName.Substring(1) + ' ' + currentDate.Year, "\0")},
+                Constants.WeekButtons
+            };
+
+            // Calendar
+            int currentDay = 1;
+            while (currentDay <= daysInMonth)
+            {
+                InlineKeyboardButton[] row = new InlineKeyboardButton[7];
+
+                for (int i = 0; i < 7; i++)
+                {
+                    if (currentDay <= daysInMonth && (i >= firstDayOfMonth || rows.Count > 2))
+                    {
+                        row[i] = InlineKeyboardButton.WithCallbackData(currentDay.ToString(), $"choose_date_{currentDay}_{currentDate.Month}_{currentDate.Year}");
+                        currentDay++;
+                    }
+                    else
+                    {
+                        row[i] = "\0";
+                    }
+                }
+                rows.Add(row);
+            }
+
+            // previous and next buttons
+            var previousMonth = currentDate.AddMonths(-1);
+            var nextMonth = currentDate.AddMonths(1);
+
+            rows.Add(new InlineKeyboardButton[] {
+                currentDate.Year >= 1111 ? InlineKeyboardButton.WithCallbackData("<<",$"ChooseMonthTimeTable_{previousMonth.Month}_{previousMonth.Year}") : "\0",
+                InlineKeyboardButton.WithCallbackData("\0", "\0"),
+                currentDate.Year <= 8888 ? InlineKeyboardButton.WithCallbackData(">>",$"ChooseMonthTimeTable_{nextMonth.Month}_{nextMonth.Year}") : "\0", });
+
+            rows.Add(new InlineKeyboardButton[] { InlineKeyboardButton.WithCallbackData("Меню", Constants.GoMenu), });
+
+            markup = new InlineKeyboardMarkup(rows);
+            SavedCalendars.Add(currentDate.Month + "_" + currentDate.Year, markup);
+            #endregion
+        }
+
+        // Send message
+        await botClient.SendTextMessageAsync(
+            message.Chat.Id,
+            "Выберите дату:",
+            replyMarkup: markup,
+            cancellationToken: cancellationToken);
+    }
+}
