37e91b10bd739bc96396a8417d662f0f9739d96c
Kolya <kolyaklimk@gmail.com>
2023-07-08 01:18:14 +0300

    Update Constants.cs

--- Timetable_Tg_Bot/Constants.cs
+++ Timetable_Tg_Bot/Constants.cs
@@ -15,8 +15,9 @@ public class Constants
     public const string ChooseMonthTimeTable = @"^TCMo_(.{1,2})_(.{4})$"; // month_year
     public const string ChooseHourTimeTable = @"^TCH_(.{1,2})_(.{1,2})_(.{4})$"; // day_month_year
     public const string ChooseMinuteTimeTable = @"^TCMi_(.{2})_(.{1,2})_(.{1,2})_(.{4})$"; // hour_day_month_year
-    public const string ChooseIsBusyTimeTable = @"^TCD_(.{2})_(.{2})_(.{1,2})_(.{1,2})_(.{4})$"; // minute_hour_day_month_year
-    public const string IsBusyTimeTable = "TIB_(.)_(.{2})_(.{2})_(.{1,2})_(.{1,2})_(.{4})$";// true\false_minute_hour_day_month_year
+    public const string ChooseIsBusyTimeTable = @"^TCIB_(.{2})_(.{2})_(.{1,2})_(.{1,2})_(.{4})$"; // minute_hour_day_month_year
+    public const string AddDescriptionTimeTable = @"^TAD_(.)_(.{2})_(.{2})_(.{1,2})_(.{1,2})_(.{4})$";// true\false_minute_hour_day_month_year
+    public const string SaveTimeTable = @"^TS_(.)_(.{2})_(.{2})_(.{1,2})_(.{1,2})_(.{4})_(\w*)$";// true\false_minute_hour_day_month_year_description
     public const string MenuDayTimeTable = @"^TMD_(.{1,2})_(.{1,2})_(.{4})$"; // day_month_year
     public const string MenuTimeTable = "TM";
     #endregion
