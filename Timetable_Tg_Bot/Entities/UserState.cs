7f0cb2425161bf9c8622fbe92cf9faaa8399744a
Kolya <kolyaklimk@gmail.com>
2023-07-08 03:04:26 +0300

    Add Buffer2

--- Timetable_Tg_Bot/Entities/UserState.cs
+++ Timetable_Tg_Bot/Entities/UserState.cs
@@ -5,5 +5,6 @@ public class UserState
     public int Id { get; set; }
     public User User { get; set; } = null!;
     public bool WaitingForText { get; set; } = false;
-    public string? Buffer { get; set; }
+    public string? Buffer1 { get; set; }
+    public int? Buffer2 { get; set; }
 }
