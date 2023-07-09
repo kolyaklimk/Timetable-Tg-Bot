12a425e79409ab19eb70fc91ff64db1ce001cdc2
Kolya <kolyaklimk@gmail.com>
2023-07-10 00:52:30 +0300

    Change Sqlite to PostgreSQL

--- Timetable_Tg_Bot/BotDbContext.cs
+++ Timetable_Tg_Bot/BotDbContext.cs
@@ -1,16 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using TimetableTgBot;
 using TimetableTgBot.Entities;
 
 public class BotDbContext : DbContext
 {
-    public DbSet<User> Users => Set<User>();
-    public DbSet<WorkTime> WorkTimes => Set<WorkTime>();
-    public DbSet<UserState> UserState => Set<UserState>();
-    public DbSet<UserBuffer> UserBuffer => Set<UserBuffer>();
+    public DbSet<User> Users { get; set; }
+    public DbSet<WorkTime> WorkTimes { get; set; }
+    public DbSet<UserState> UserState { get; set; }
+    public DbSet<UserBuffer> UserBuffer { get; set; }
     public BotDbContext() => Database.EnsureCreated();
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlite($"Data Source=bot.db");
+        optionsBuilder.UseNpgsql(PrivateConstants.DB_TOKEN);
     }
 }
