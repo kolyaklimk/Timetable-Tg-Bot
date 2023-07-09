af06fb080412d7624c8fe069dacd7a6e3817f0a3
Kolya <kolyaklimk@gmail.com>
2023-07-09 13:21:47 +0300

    Add UserBuffer

--- Timetable_Tg_Bot/Entities/UserBuffer.cs
+++ Timetable_Tg_Bot/Entities/UserBuffer.cs
@@ -1,9 +1,11 @@
 ﻿namespace TimetableTgBot.Entities;
 
-public class UserState
+public class UserBuffer
 {
     public long Id { get; set; }
     public long UserId { get; set; }
     public User User { get; set; } = null!;
-    public bool WaitingForText { get; set; } = false;
+    public string? Buffer1 { get; set; }
+    public int? Buffer2 { get; set; }
+    public string? Buffer3 { get; set; }
 }
