98434caaeecd96d2715ced1c798673c64e5827e8
Kolya <kolyaklimk@gmail.com>
2023-07-19 18:29:43 +0300

    Update Db

--- Timetable_Tg_Bot/Entities/User.cs
+++ Timetable_Tg_Bot/Entities/User.cs
@@ -7,5 +7,6 @@ public class User
     public string? LastName { get; set; }
     public string? UserName { get; set; }
     public ICollection<WorkTime> WorkTimes { get; } = new List<WorkTime>();
+    public ICollection<TimeTableTemplate> TimeTableTemplates { get; } = new List<TimeTableTemplate>();
     public DateTime Subscription { get; set; }
 }
--- Timetable_Tg_Bot/Entities/WorkTime.cs
+++ Timetable_Tg_Bot/Entities/WorkTime.cs
@@ -5,6 +5,8 @@ public class WorkTime
     public long Id { get; set; }
     public long UserId { get; set; }
     public User User { get; set; } = null!;
+    public long? TimeTableTemplateId { get; set; }
+    public TimeTableTemplate? TimeTableTemplate { get; set; }
     public TimeOnly Start { get; set; }
     public DateOnly Date { get; set; }
     public bool IsBusy { get; set; }
