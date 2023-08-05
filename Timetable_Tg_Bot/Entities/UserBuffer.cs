a0eba0ef154fd378d06f2e8155707c899d2156a0
Kolya <kolyaklimk@gmail.com>
2023-08-05 19:20:28 +0300

    Now UserBufferId == UserId

--- Timetable_Tg_Bot/Entities/UserBuffer.cs
+++ Timetable_Tg_Bot/Entities/UserBuffer.cs
@@ -2,9 +2,7 @@
 
 public class UserBuffer
 {
-    public long Id { get; set; }
-    public long UserId { get; set; }
-    public User User { get; set; } = null!;
+    public long Id { get; set; } // Id == UserId
     public bool WaitingForText { get; set; } = false;
     public string? Buffer1 { get; set; }
     public int? Buffer2 { get; set; }
