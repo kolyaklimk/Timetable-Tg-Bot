73dabe6e9fb83ea356004391f54c23fef87fe67e
Kolya <kolyaklimk@gmail.com>
2023-08-14 17:48:02 +0300

    Add WaitingForDocument and rename Buffer2

--- Timetable_Tg_Bot/Entities/UserBuffer.cs
+++ Timetable_Tg_Bot/Entities/UserBuffer.cs
@@ -4,8 +4,9 @@ public class UserBuffer
 {
     public long Id { get; set; } // Id == UserId
     public bool WaitingForText { get; set; } = false;
+    public bool WaitingForDocument { get; set; } = false;
     public string? Buffer1 { get; set; }
-    public int? Buffer2 { get; set; }
+    public int? MessageId { get; set; }
     public string? Buffer3 { get; set; }
     public List<DateOnly> ImageDays { get; set; } = new(); // 1 - first day; 2 - last day; Next - deleted days
 }
